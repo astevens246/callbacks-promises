8758f6f5e8ab333a1fadcae7783ef13c5f737823
Allen Stevens <allen.stevens@students.dominican.edu>
2024-06-11 15:47:36 -0700

    completed promise 8

--- Promise-8/index.js
+++ Promise-8/index.js
@@ -9,7 +9,7 @@ const promise1 = new Promise((resolve, reject) => {
 
 const promise2 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("Second promise resolved");
+    reject("Second promise resolved");
   }, 2000);
 });
 
@@ -19,25 +19,36 @@ const promise3 = new Promise((resolve, reject) => {
   }, 3000);
 });
 
-Promise.all([promise1, promise2, promise3]).then(results => {
+const promise4 = new Promise((resolve, reject) => { 
+  setTimeout(() => {
+    resolve("Fourth promise resolved");
+  }, 10);
+});
+
+Promise.all([promise1, promise2, promise3, promise4]).then(results => {
   console.log(results);
+})
+.catch(error => {
+  console.error('Error:', error);
 });
 
 
 // Challenges:
 
 // Test the code above. What happened? Explain below: 
-
+// Promise.all waits for all promises to resolve before it resolves.
 
 // Add another Promise to the Array of promises (line 22)
 // make your promise resolve in 10ms. Test your work. 
 // How long did it take for all of the promises to resolve?
-
+// it took 3 seconds becuase the longest promise took 3 seconds to resolve
 
 // Make one of the Promises fail by calling reject. What happens? 
-
+// The catch block is executed
 
 // Add a catch() block to the end of `Promise.all().then()`. 
 // Handle your error here.
 
+
 // What happens when one Promise fails? 
+// They all fail 
